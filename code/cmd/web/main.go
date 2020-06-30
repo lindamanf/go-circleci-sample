13f4cf11710bfcda1c826ab77f7e27b95784047d
lindamanf <lindaman.h.12@outlook.jp>
2020-06-30 09:27:13 +0900

    first commit

---
+++ code/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+func hoge(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hogehoge")
+}
+func top(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+func main() {
+	http.HandleFunc("/", top)
+	http.HandleFunc("/hoge", hoge)
+	http.ListenAndServe(":8080", nil)
+}
